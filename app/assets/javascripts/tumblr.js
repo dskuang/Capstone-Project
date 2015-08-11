52d6b6bef1843440976258822b60a1ce9ddcaf65
dominic_kuang <dskuang@gmail.com>
2015-08-10 19:39:04 -0700

    finished auth

---
+++ app/assets/javascripts/tumblr.js
@@ -0,0 +1,13 @@
+window.Tumblr = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Tumblr.initialize();
+});
