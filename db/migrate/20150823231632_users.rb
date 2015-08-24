0bb445b6d5e6ef46c88076c3b3bea627dfd2776a
dominic_kuang <dskuang@gmail.com>
2015-08-23 21:46:00 -0700

    fixes

--- db/migrate/20150823231632_users.rb
+++
@@ -1,8 +0,0 @@
-class Users < ActiveRecord::Migration
-  def change
-    add_column :users, :firstname, :string
-    add_column :users, :lastname, :string
-    add_column :users, :city, :string
-    add_column :users, :state, :string
-  end
-end
